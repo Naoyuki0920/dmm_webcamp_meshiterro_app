00bf7b8b7bc1a8d01aada74408184d9a0f0fac2a
Naoyuki0920 <naoyuki-effort0920@docomo.ne.jp>
2021-02-15 13:07:12 +0000

    Finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3e7d1067ee3c254b2d0b717de0b39cbcd8c8e5b93dbd9420e465c38bd2908248f67492e3f46354948b3d641d8d3098670a0851ca37af1bbd42f905709c1eaf40'
---
+++ db/migrate/20210213094456_add_profilerails.rb
@@ -0,0 +1,4 @@
+class AddProfilerails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
